7293ab1be082aa525bde03a9554ee853910de8ce
Harley Ferguson <harley@haefelesoftware.com>
2021-04-16 20:35:09 +0200

    Initial commit.

---
+++ lib/flutter_text_field_fab.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+class TextFieldFloatingActionButton extends StatefulWidget {
+  final String label;
+  final void Function(String) onChange;
+  final void Function(String) onSubmit;
+  final Function onClear;
+
+  TextFieldFloatingActionButton(this.label,
+      {this.onChange, this.onSubmit, this.onClear});
+
+  @override
+  _TextFieldFloatingActionButtonState createState() =>
+      _TextFieldFloatingActionButtonState(label,
+          onChange: onChange, onSubmit: onSubmit, onClear: onClear);
+}
+
+class _TextFieldFloatingActionButtonState
+    extends State<TextFieldFloatingActionButton> {
+  final String label;
+  final void Function(String) onChange;
+  final void Function(String) onSubmit;
+  final Function onClear;
+
+  bool folded = true;
+
+  _TextFieldFloatingActionButtonState(this.label,
+      {this.onChange, this.onSubmit, this.onClear});
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+        duration: Duration(milliseconds: 400),
+        width: folded ? 56 : MediaQuery.of(context).size.width / 1.5,
+        height: 56,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(32),
+            color: Colors.white,
+            boxShadow: kElevationToShadow[6]),
+        child: Row(
+          children: [
+            Expanded(
+                child: Container(
+                    padding: EdgeInsets.only(left: 16),
+                    child: !folded
+                        ? TextField(
+                            decoration: InputDecoration(
+                                hintText: label,
+                                hintStyle: TextStyle(
+                                    color: Theme.of(context).accentColor),
+                                border: InputBorder.none),
+                            onChanged: (String query) => onChange(query),
+                          )
+                        : null)),
+            Container(
+              child: Material(
+                type: MaterialType.transparency,
+                child: InkWell(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(folded ? 32 : 0),
+                    topRight: Radius.circular(32),
+                    bottomLeft: Radius.circular(folded ? 32 : 0),
+                    bottomRight: Radius.circular(32),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: Icon(
+                      folded ? Icons.search : Icons.close,
+                      color: Theme.of(context).accentColor,
+                    ),
+                  ),
+                  onTap: () {
+                    setState(() {
+                      if (!folded) {
+                        onClear();
+                      }
+                      folded = !folded;
+                    });
+                  },
+                ),
+              ),
+            ),
+          ],
+        ));
+  }
+}
